ee35e8cdbf24d29fa74a31350c4b4956a9f93274
Carl Hörberg <carl.hoerberg@gmail.com>
2011-09-13 10:45:44 +0200

    Investigating what type of object is return in the lock mechanism of the redis engine

--- lib/faye/engines/redis.rb
+++ lib/faye/engines/redis.rb
@@ -180,7 +180,8 @@ module Faye
             block.call(release_lock)
           else
             @redis.get(lock_key) do |timeout|
-              lock_timeout = timeout.to_i
+              puts timeout.class
+              lock_timeout = timeout.to_i(10)
               if lock_timeout < current_time
                 @redis.getset(lock_key, expiry) do |old_value|
                   block.call(release_lock) if old_value == timeout
